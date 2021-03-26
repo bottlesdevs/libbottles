66497b2352121e1e6aa2718f60984beef9294b45
Mirko Brombin <send@mirko.pm>
2021-03-26 16:34:53 +0100

    Initial manager structure

---
+++ libbottles/manager.py
@@ -0,0 +1,39 @@
+def list_bottles(extra_paths: list = []):
+    '''
+    List user bottles.
+
+    Parameters
+    ----------
+    extra_paths : str (optional)
+        add extra paths to the search
+
+    Return
+    ----------
+    list:
+        a list of Bottle objects
+    '''
+    return
+
+
+def list_runners():
+    '''
+    List local runners.
+
+    Return
+    ----------
+    list:
+        a list of Runner objects
+    '''
+    return
+
+
+def list_dxvk():
+    '''
+    List local DXVK.
+
+    Return
+    ----------
+    list:
+        a list of DXVK objects
+    '''
+    return
