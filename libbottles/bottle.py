e8f783c0d6af7428a5cb2a8deade5654edc110b6
Mirko Brombin <send@mirko.pm>
2021-03-28 15:06:45 +0200

    validate_bottle method

--- libbottles/bottle.py
+++ libbottles/bottle.py
@@ -1,3 +1,6 @@
+import json
+from glob import glob
+
 class Bottle:
     '''
     Create a new object of type Bottle with all the methods for its management.
@@ -6,8 +9,6 @@ class Bottle:
     ----------
     path : str
         the bottle full path
-    generate : bool (optional)
-        generate config file if missing in path (default is False)
     '''
     name = str
     path = str
@@ -61,17 +62,84 @@ class Bottle:
         "Programs": {}
     }
 
-    def __init__(self, path: str, generate: bool = False):
-        if not self.validate_bottle(path):
+    def __init__(self, path: str):
+        self.path = path
+        if not self.validate_bottle():
             raise ValueError("Given path doesn't seem a valid Bottle path.")
 
     def validate_bottle(self):
         '''
-        TODO: validate_bottle()
-        - check if config doens't exists in path
-            - if generate == True: create config file in path
-            - else: return False
-        - check for update in config comparing with config_struct
-            - if True: update
+        Check if it is a valid bottle path.
+
+        Return
+        ----------
+        bool
+            True if it is a valid bottle path, otherwise False
         '''
-        return
+        promise = ["dosdevices", "drive_c"]
+
+        dirs = glob(f"{self.path}/*")
+        dirs = [d.replace(f"{self.path}/", "") for d in dirs]
+
+        for p in promise:
+            if p not in dirs:
+                return False
+
+        '''
+        Load config from path, if it not exists then create.
+        '''
+        try:
+            file = open(f"{self.path}/bottle.json")
+            config = json.load(file)
+            file.close()
+        except FileNotFoundError:
+            file = open(f"{self.path}/bottle.json", "w")
+            config = self.config_struct
+            config["Name"] = self.path.capitalize()
+            config["Path"] = self.path
+            # TODO: set runner to last installed
+            json.dump(config, file, indent=4)
+            config = json.load(file)
+            file.close()
+
+        '''
+        Check for diffs. between config_struct and the bottle one, then update.
+        '''
+        missing_keys = self.config_struct.keys() - config.keys()
+        for key in missing_keys:
+            self.update_config(
+                key=key,
+                value=self.config_struct[key]
+            )
+
+        missing_keys = self.config_struct["Parameters"].keys() - config["Parameters"].keys()
+        for key in missing_keys:
+            self.update_config(
+                key=key,
+                value=self.config_struct[key],
+                scope="Parameters"
+            )
+            
+        return True
+
+    def update_config(self, key:str, value:str, scope:str=None):
+        '''
+        Update keys for a bottle config file.
+
+        Parameters
+        ----------
+        key : str
+            the key name
+        value : str
+            the value to be set
+        scope : str (optional)
+            where to look for the key if it is not the root (default is None)
+        '''
+        if scope is not None:
+            self.config[scope][key] = value
+        else:
+            self.config[key] = value
+
+        file = open(f"{self.path}/bottle.json", "w")
+        json.dump(self.config, file, indent=4)
+        file.close()
