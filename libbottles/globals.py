d84a24841619eb838f4c6a82fec356915b6f71f7
Mirko Brombin <send@mirko.pm>
2021-04-07 14:15:03 +0200

    TODO: _base should change if flatpak or snap

--- libbottles/globals.py
+++ libbottles/globals.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 
 class Paths:
+    # TODO: _base should change if flatpak or snap
     _base = f"{Path.home()}/.local/share/bottles"
     bottles = f"{_base}/bottles"
     components = f"{_base}/components"
