265880de1551851e0f13f125c61f217de4e7e82e
Mirko Brombin <send@mirko.pm>
2021-04-24 17:17:40 +0200

    Add package setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='libbottles',
+    version='0.1',
+    license='GPL-3.0',
+    description='A python library for interacting with Bottles.',
+    keywords='libbottles bottles wine proton windows',
+
+    url='https://github.com/bottlesdevs/libbottles',
+
+    author='Mirko Brombin',
+    author_email='send@mirko.pm',
+
+    packages=find_packages()
+)
