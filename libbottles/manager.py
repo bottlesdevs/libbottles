a521f374aef1c016a06257f3178907b0c3e40fac
Mirko Brombin <send@mirko.pm>
2021-04-25 15:22:23 +0200

    Using static class for Manager

--- libbottles/manager.py
+++ libbottles/manager.py
@@ -1,81 +1,120 @@
 from glob import glob
+from os import environ
+
+from libwine.wine import Wine
+from libwine.proton import Proton
+
 from bottle import Bottle
 from components.runner import Runner
 import globals
 
 
-def list_bottles(paths: list = [globals.Paths.bottles]):
-    '''
-    List user bottles.
+class Manager:
 
-    Parameters
-    ----------
-    paths : list
-        paths to search for bottles
+    _bottles = []
+    _components = []
+    _environments = [
+        "Software",
+        "Gaming",
+        "Custom"
+    ]
 
-    Return
-    ----------
-    list:
-        a list of Bottle objects
-    '''
-    results = []
-    for p in paths:
-        results += glob(f"{p}/*", recursive=True)
+    @staticmethod
+    def update_bottles(paths: list = [globals.Paths.bottles]):
+        '''
+        Update user bottles.
 
-    bottles = []
-    for r in results:
-        try:
-            bottle = Bottle(r)
-            bottles.append(bottle)
-        except ValueError:
-            continue
+        Parameters
+        ----------
+        paths : list
+            paths to search for bottles
+        '''
+        results = []
+        for p in paths:
+            results += glob(f"{p}/*", recursive=True)
+            
+        for r in results:
+            try:
+                bottle = Bottle(r)
+                Manager._bottles.append(bottle)
+            except ValueError:
+                continue
 
-    return bottles
+    @staticmethod
+    def get_bottles():
+        '''
+        Get user bottles.
+
+        Return
+        ----------
+        list:
+            a list of Bottle objects
+        '''
+        return Manager._bottles
+
+    @staticmethod
+    def create_bottle(environment: int, path: str, name: str, runner: Runner, runner_path: str, versioning: bool, verbose: int):
+        if not Manager._environments[environment]:
+            raise ValueError(f"{environment} is not a valid environment.")
+        
+        config = {
+            "Name": name,
+            "Runner": runner_path,
+            "Path": path,
+            "Versioning": versioning,
+            "Verbose": verbose
+        }
+
+        wineprefix = runner(
+            winepath=runner_path,
+            wineprefix=path
+        )
+        wineprefix.update()
+        
+        bottle = Bottle(path)
+        bottle.apply_config(config)
+        bottle.apply_environment(environment)
+
+        Manager._bottles.append(bottle)
+        return bottle
 
 
-'''
-print(
-    list_bottles()[0].config["Parameters"]
-)
-'''
+    # TODO: old code starts from here
 
+    def list_runners(paths: list = [globals.Paths.runners]):
+        '''
+        List local runners.
 
-def list_runners(paths: list = [globals.Paths.runners]):
-    '''
-    List local runners.
+        Return
+        ----------
+        list:
+            a list of Runner objects
+        '''
+        results = []
+        for p in paths:
+            results += glob(f"{p}/*", recursive=True)
 
-    Return
-    ----------
-    list:
-        a list of Runner objects
-    '''
-    results = []
-    for p in paths:
-        results += glob(f"{p}/*", recursive=True)
+        runners = []
+        for r in results:
+            try:
+                runner = Runner(r)
+                runners.append(runner)
+            except ValueError:
+                continue
 
-    runners = []
-    for r in results:
-        try:
-            runner = Runner(r)
-            runners.append(runner)
-        except ValueError:
-            continue
+        return runners
 
-    return runners
+    print(
+        list_runners()
+    )
 
+    def list_dxvk():
+        '''
+        List local DXVK.
 
-print(
-    list_runners()
-)
-
-
-def list_dxvk():
-    '''
-    List local DXVK.
-
-    Return
-    ----------
-    list:
-        a list of DXVK objects
-    '''
-    return
+        Return
+        ----------
+        list:
+            a list of DXVK objects
+        '''
+        return
