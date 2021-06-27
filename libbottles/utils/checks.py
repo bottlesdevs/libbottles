921bf0ede85594893c6a08584e621ad3716878e6
mirkobrombin <send@mirko.pm>
2021-06-27 09:12:27 +0200

    check_special_chars method

---
+++ libbottles/utils/checks.py
@@ -0,0 +1,20 @@
+import re
+
+
+def check_special_chars(text: str) -> bool:
+    """
+    Check for special characters in text.
+
+    Parameters
+    ----------
+    text : str
+        the text to be checked
+
+    Return
+    ----------
+        returns True if it finds a special character, else False
+    """
+    regex = re.compile('[@!#$%^&*()<>?/|}{~:.;,]')
+    if regex.search(text) is None:
+        return False
+    return True
